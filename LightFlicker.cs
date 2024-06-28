dd60cfe500d808f793e6bb5ce8b899ff7692d097
LousyBook-01 <lousybook94@gmail.com>
2024-06-28 21:08:37 +0530

    Woah

---
+++ LightFlicker.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    public float minLight = 0;
+    public float maxLight = 1;
+    public float newLightDelay = 0.05f;
+    public float transitionSpeed = 0.2f;
+    public Light light;
+
+    private float time = 0;
+    private float target;
+
+    void Start()
+    {
+        target = Random.Range(minLight,maxLight);
+    }
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time > newLightDelay) {
+            target = Random.Range(minLight,maxLight);
+            time = 0;
+        }
+        light.intensity = Mathf.Lerp(light.intensity, target, transitionSpeed * Time.deltaTime * Time.timeScale);
+        Ra
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Unity Light Flicker
- simple unity light flicker script
+ simple unity light flicker 
+# Usage
+- Import the script and drag it onto some object
+- Set the values as necessary, you need to select the light object and drag and drop it into the "Light" variable
+- Done ig?
